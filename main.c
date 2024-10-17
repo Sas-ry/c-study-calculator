e42d762b5e3976396cc69ef92dbcd08a1d1e9d4e
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-10-18 01:16:48 +0900

    step3 done

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int main(void) {
     printf("数字を入れて下さい\n");
     char in_num_char[256];
     scanf("%255s", &in_num_char);
-    int in_num = in_num_char[0] - '0'; 
+    int i = 0;
+    int in_num = 0;
+    for(i = 0; '0' <= in_num_char[i] && '9' >= in_num_char[i]; i++) {
+        in_num = in_num * 10 + in_num_char[i] - '0'; 
+    }
     printf("入力された値は：%d\n", in_num);
 
 }
