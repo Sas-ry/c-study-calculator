eef862b7e7af1000742e7fb4e7aef46b78900ed7
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-10-18 00:41:57 +0900

    step2 done

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     printf("数字を入れて下さい\n");
-    int in_num;
-    scanf("%d", &in_num);
-
+    char in_num_char[256];
+    scanf("%255s", &in_num_char);
+    int in_num = in_num_char[0] - '0'; 
     printf("入力された値は：%d\n", in_num);
 
-    return 0;
 }
