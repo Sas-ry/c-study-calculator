4804cbaeff0898784b8a8143d19e1e7838b3d7b1
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-10-18 01:31:10 +0900

    step4 done

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+int load_point = 0;
+char in_num_char[256];
+
+int analyze_number(void) {
+    int load_result = 0;
+    for(load_point = 0; '0' <= in_num_char[load_point] && '9' >= in_num_char[load_point]; load_point++) {
+        load_result = load_result * 10 + in_num_char[load_point] - '0'; 
+    }
+    
+    return load_result;
+}
+
 int main(void) {
     printf("数字を入れて下さい\n");
-    char in_num_char[256];
     scanf("%255s", &in_num_char);
-    int i = 0;
-    int in_num = 0;
-    for(i = 0; '0' <= in_num_char[i] && '9' >= in_num_char[i]; i++) {
-        in_num = in_num * 10 + in_num_char[i] - '0'; 
-    }
-    printf("入力された値は：%d\n", in_num);
+    int load_result = analyze_number();
+    printf("入力された値は：%d\n", load_result);
 
 }
