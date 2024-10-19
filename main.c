89e935df4a1f91adcff3516155879f37ca4a5edc
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-10-20 01:15:27 +0900

    step12 done

--- main.c
+++ main.c
@@ -10,6 +10,18 @@ int analyze_number(void) {
     for(i = 0; '0' <= in_num_char[load_point] && '9' >= in_num_char[load_point]; load_point++) {
         load_result = load_result * 10 + in_num_char[load_point] - '0'; 
     }
+    if (in_num_char[load_point] == '+') {
+        load_point++;
+        for(i = 0; '0' <= in_num_char[load_point] && '9' >= in_num_char[load_point]; load_point++) {
+            load_result = load_result * 10 + in_num_char[load_point] - '0'; 
+        }
+    } else if (in_num_char[load_point] == '-') {
+        load_point++;
+        for(i = 0; '0' <= in_num_char[load_point] && '9' >= in_num_char[load_point]; load_point++) {
+            load_result = load_result * 10 + in_num_char[load_point] - '0'; 
+        }
+        load_result *= -1;
+    }
     
     return load_result;
 }
@@ -47,7 +59,6 @@ int calculation_add_sub(void) {
 int main(void) {
     printf("数字を入れて下さい\n");
     scanf("%255s", &in_num_char);
-    int load_result = calculation_add_sub();
+    int load_result = analyze_number();
     printf("入力された値は：%d\n", load_result);
-
 }
