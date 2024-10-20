d193e37dd12ecf4e7c7a7f7ab0544a91d0209150
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-10-20 15:17:36 +0900

    step14 done

--- main.c
+++ main.c
@@ -58,9 +58,23 @@ int calculation_add_sub(void) {
     return load_result;
 }
 
+int analyze_parentheses(void) {
+    int load_result;
+    if (in_num_char[load_point] == '(') {
+        load_point++;
+        load_result = calculation_add_sub();
+        if (in_num_char[load_point] == ')') {
+            load_point++;
+        }
+    } else {
+        load_result = analyze_number();
+    }
+    return load_result;
+}
+
 int main(void) {
     printf("数字を入れて下さい\n");
     scanf("%255s", &in_num_char);
-    int load_result = calculation_add_sub();
+    int load_result = analyze_parentheses();
     printf("入力された値は：%d\n", load_result);
 }
